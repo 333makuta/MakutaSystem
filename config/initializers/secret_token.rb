dd6543f241fb9181d5bb878b45fdcbcea9ea89da
YukioMakuta <yukio.makuta@kym.biglobe.ne.jp>
2013-12-30 05:39:12 +0000

    Generate a new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MakutaSystem::Application.config.secret_key_base = 'ac0fd4ba641993450c11e03fad287e03032887704557a82c3f0a7e6f1c79cb17238d3257dbcbb2cf0b6c3a7bf3ffcd3439ea1709e1cc29d79ab05541a0729a3d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MakutaSystem::Application.config.session_store :cookie_store, key: '_MakutaSystem_session'
